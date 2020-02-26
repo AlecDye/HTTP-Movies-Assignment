452ae858a4ea85b15befe578796a41aa2576dd3b
Alec Dye <alecdye@gmail.com>
2020-02-26 15:26:09 -0800

    building out movie form

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { Route } from "react-router-dom";
+
+import MovieForm from "./Movies/MovieForm";
 import SavedList from "./Movies/SavedList";
 import MovieList from "./Movies/MovieList";
 import Movie from "./Movies/Movie";
@@ -36,7 +38,9 @@ const App = () => {
         <Movie addToSavedList={addToSavedList} />
       </Route>
 
-      <Route path="/update-movie/:id" />
+      <Route path="/update-movie/:id">
+        <MovieForm />
+      </Route>
     </>
   );
 };
