00f14209153a9054f621f035a819f2d82112fb14
Alec Dye <alecdye@gmail.com>
2020-02-26 18:57:58 -0800

    adding handleSubmit and initalState

--- client/src/Movies/MovieForm.js
+++ client/src/Movies/MovieForm.js
@@ -1,11 +1,27 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 
+const initialMovie = {
+    id: null,
+    title: "",
+    director: "",
+    metascore: null,
+    stars: ""
+}
+
 const MovieForm = () => {
     const [editMovie, setEditMovie] = useState([]);
 
+    const handleSubmit = e => {
+        e.preventDefault();
+    }
+    //todo: handleSubmit
+    //todo: handleChanges
+    //todo: axios put request
+
+
     return (
-        <form>
+        <form onSubmit={handleSubmit}>
             <input />
             <button>Update</button>
         </form>
