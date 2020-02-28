f267423d912b2cb555b9bfa641f54237c18f53e3
Alec Dye <alecdye@gmail.com>
2020-02-27 17:17:17 -0800

    updaing to stable version

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,5 @@
 import React, { useState, useEffect } from "react";
 import { Route } from "react-router-dom";
-
-import MovieForm from "./Movies/MovieForm";
 import SavedList from "./Movies/SavedList";
 import MovieList from "./Movies/MovieList";
 import Movie from "./Movies/Movie";
