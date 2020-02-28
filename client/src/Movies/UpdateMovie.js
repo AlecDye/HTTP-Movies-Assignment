d6049a77fc2f1d28f790ba72885f2acfda534161
Alec Dye <alecdye@gmail.com>
2020-02-27 16:30:33 -0800

    building out Update component

---
+++ client/src/Movies/UpdateMovie.js
@@ -0,0 +1,20 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import { useRouteMatch, useHistory } from "react-router-dom";
+
+const initialMovie = {
+    id: "",
+    title: "",
+    director: "",
+    metascore: "",
+    stars: []
+}
+
+const UpdateMovie = () => {
+    return (
+        <>
+        </>
+    )
+}
+
+export default UpdateMovie;
